55aea1da76ab4a53ba36fe65f396327273b7669b
Yogesh Rathore <yogeshrathore251@gmail.com>
2023-12-29 20:08:56 +0530

    First commit

---
+++ InstagramCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by Yogesh Rathore on 29/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
